6953a1cffec356dec5017a753b6ab0a761a70a57
Xerx81 <ryuvraj832@gmail.com>
2025-01-27 15:15:48 +0530

    added translation feature

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import warnings
 import whisper
+from googletrans import Translator
 
 # Ignore warnings
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -25,29 +26,44 @@ def download_audio(video_url, output_file="audio.mp3"):
         return None
 
 
-def transcribe(audio_file):
+def transcribe_and_translate(audio_file, target_language):
     try:
         model = whisper.load_model("base")
         print("Transcribing... Please wait.")
         result = model.transcribe(audio_file)
-        return result['text']
+        transcription = result['text']
+
+        # Translate the transcription to the target language
+        print(f"Translating to {target_language}... Please wait.")
+        translator = Translator()
+        translation = translator.translate(transcription, dest=target_language).text
+        
+        return transcription, translation
+
 
     except Exception as e:
-        print(f"Error transcripting file: {e}")
-        return None
+        print(f"Error: {e}")
+        return None, None
 
 
 if __name__ == "__main__":
     video_url = input("Enter the video link: ")
+    target_language = input("Enter language key for translation: ")
 
     audio_file = download_audio(video_url, "audio.mp3")  
-    transcripted_file = transcribe(audio_file)
+    transcripted_file, translation = transcribe_and_translate(audio_file, target_language)
 
     if transcripted_file:
         output_txt = "transcription.txt"
         with open(output_txt, "w") as file:
             file.write(transcripted_file)
         print(f"Transcription saved to \"{output_txt}\"")
+
+    if translation:
+        output_txt = "translation.txt"
+        with open(output_txt, "w") as file:
+            file.write(translation)
+        print(f"Translation saved to \"{output_txt}\"")
 
     if audio_file:
         os.remove(audio_file)
